3314d187b119c07b9718eab8d28aae1631907d44
Pranto Mollick <prantocse01@gmail.com>
2021-06-11 23:26:00 +0600

    login system implement

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app.forms import LoginForm
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -23,10 +24,22 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_aunticated:
+        return redirect(url_for('index'))
     form = LoginForm()
-
     if form.validate_on_submit():
-        flash("Login requested for user {}, remember_me = {}".format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index')), 301
+        user = User.query.filter_by(username= form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid Username or password")
+            return redirect(url_for('login')), 301
+        login_user(user, remember=form.remember_me.data) 
+        return redirect(url_for('index'))
+        # flash("Login requested for user {}, remember_me = {}".format(form.username.data, form.remember_me.data))
+        # return redirect(url_for('index')), 301
 
     return render_template('login.html', title = 'Sign In', form = form), 200
+
+@app.route('/logout')
+def logout(): 
+    logout_user()
+    return redirect(url_for('index')), 301
