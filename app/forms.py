cae3601c1e84897ab755f03cbdcc5ba4210dde88
Pranto Mollick <prantocse01@gmail.com>
2021-06-13 02:09:37 +0600

    register forms add with his view function add

--- app/forms.py
+++ app/forms.py
@@ -3,10 +3,12 @@
 A form class simply defines the fields of the form as class variables.
 """
 
+from re import sub
 from flask_wtf import FlaskForm
 from flask_wtf.recaptcha import validators
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=5, max=10)])
@@ -14,3 +16,21 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
 
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different Username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
-from app.forms import LoginForm
-from flask import render_template, flash, redirect, url_for
-from app import app
-from flask_login import current_user, login_user, logout_user
+from app.forms import LoginForm, RegistrationForm
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from flask_login import current_user, login_user, logout_user, login_required, AnonymousUserMixin
 from app.models import User
+from werkzeug.urls import url_parse
+
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username' : 'Pranto'}
     posts = [
@@ -22,24 +25,61 @@ def index():
     ]
     return render_template('index.html', title = 'Home', user=user, posts=posts), 200
 
+
+# Login handaller
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_aunticated:
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(username= form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash("Invalid Username or password")
             return redirect(url_for('login')), 301
         login_user(user, remember=form.remember_me.data) 
+        """
+        /index, for example, the @login_required decorator will intercept the
+        request and respond with a redirect to /login, but it will add a query
+        string argument to this URL, making the complete redirect URL
+        /login?next=/index. The next query string argument is set to the
+        original URL, so the application can use that to redirect back after
+        login.
+        """
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page), 301
+
         return redirect(url_for('index'))
         # flash("Login requested for user {}, remember_me = {}".format(form.username.data, form.remember_me.data))
         # return redirect(url_for('index')), 301
 
     return render_template('login.html', title = 'Sign In', form = form), 200
 
+
+#logout handler
 @app.route('/logout')
 def logout(): 
     logout_user()
     return redirect(url_for('index')), 301
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index')), 301
+    
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        print("Pass")
+        user = User(username = form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login')), 301
+    
+    return render_template('register.html', title='Register', form=form), 200
